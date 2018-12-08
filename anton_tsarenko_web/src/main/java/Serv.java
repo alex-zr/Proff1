9e0d34f53f2afc02f67578e4520f31d289785535
Anton Tsarenko <tsarenkoanton.oleksandrovich@gmail.com>
2018-12-08 14:59:29 +0200

    First servlet

---
+++ anton_tsarenko_web/src/main/java/Serv.java
@@ -0,0 +1,15 @@
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "serv", urlPatterns = "/")
+public class Serv extends HttpServlet{
+    @Override
+    public void doGet(HttpServletRequest req,
+                      HttpServletResponse resp) throws IOException {
+        resp.getWriter().println("<h1> Hello from Server</h1>");
+    }
+}
---
+++ anton_tsarenko_web/src/main/webapp/index.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: anton
+  Date: 12/7/18
+  Time: 4:43 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+    <h2>Welcome to this site!</h2>
+    <h3>This site is a beginning of my history </h3>
+    <a href="/serv">You can look at my first servlet</a>
+</body>
+</html>
