71a8d30fcaf582d33658c96b2b82e112ea4a8ed9
BrYarik <brodovskyyaroslav@gmail.com>
2018-12-11 16:54:12 +0200

    added first web aplication

---
+++ proff1_brodovsky_web/src/main/java/prog/kiev/ua/Serv.java
@@ -0,0 +1,15 @@
+package prog.kiev.ua;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/serv")
+public class Serv extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.getWriter().println("<h1> Hello from serv </h1>");
+    }
+}
---
+++ proff1_brodovsky_web/src/main/webapp/index.jsp
@@ -0,0 +1,12 @@
+<html>
+<body>
+    <h2>Hello World!</h2>
+    <o1>
+        <li>Lexus</li>
+        <li>BMW</li>
+        <li>Infinity</li>
+    </o1>
+
+    <a href="/serv">Go to first servlet</a>
+</body>
+</html>
