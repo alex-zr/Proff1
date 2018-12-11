a5db197064c326f7c7b1fc756468884fe91322e5
Viktor Husakivskiy <viktorgusakivskii@gmail.com>
2018-12-11 20:43:32 +0200

    Add new servlet

---
+++ proff1_try_web/src/main/Java/ua/liev/prog/serv.java
@@ -0,0 +1,9 @@
+package Java.ua.liev.prog;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet(name="serv",urlPatterns = "/serv")
+public class serv extends HttpServlet {
+
+}
