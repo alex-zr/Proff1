12b4e85a240d34cfa7cdf59a07e0f0c774ab8900
AlinaSergeeva6 <43118734+AlinaSergeeva6@users.noreply.github.com>
2019-01-10 21:21:08 +0200

    added my module

--- alina_sergeeva_web/src/main/java/ua/kiev/prog/Serv.java
+++ alina_sergeeva_web/src/main/java/ua/kiev/prog/Serv.java
@@ -5,12 +5,66 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+
 
 @WebServlet(name = "serv", urlPatterns = "/")
 public class Serv extends HttpServlet {
-    @Override
-    public void doGet(HttpServletRequest req,
-                      HttpServletResponse resp) throws IOException {
-        resp.getWriter().println("<h1> Hello from Serv </h1>");
+
+    static final int OS_WINDOWS = 0;
+    static final int OS_LINUX = 1;
+    static final int OS_MacOS = 2;
+
+    static final int PHONE_SAMSUNG = 3;
+    static final int PHONE_IPHONE = 4;
+    static final int PHONE_MEIZU = 5;
+    static final int PHONE_LENOVO = 6;
+    static final int PHONE_OTHER = 7;
+
+    static final String TEMPLATE = "<html><head><title>STATICTIC</title></head>" +
+            "<body>%s</body></html>";
+
+    final int[] results = new int[8];
+
+
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+
+
+        final String os = req.getParameter("os");
+        final String phone = req.getParameter("phone");
+
+        if ("windows".equals(os)) {
+            results[OS_WINDOWS]++;
+        }
+        if ("linux".equals(os)) {
+            results[OS_LINUX]++;
+        }
+        if ("macOs".equals(os)) {
+            results[OS_MacOS]++;
+        }
+
+
+        if ("samsung".equals(phone)) {
+            results[PHONE_SAMSUNG]++;
+        }
+        if ("iPhone".equals(phone)) {
+            results[PHONE_IPHONE]++;
+        }
+        if ("meizu".equals(phone)) {
+            results[PHONE_MEIZU]++;
+        }
+        if ("lenovo".equals(phone)) {
+            results[PHONE_LENOVO]++;
+        }
+        if ("other".equals(phone)) {
+            results[PHONE_OTHER]++;
+        }
+
+        String res = "<p>Windows: " + results[OS_WINDOWS] + "<br>Linux: " + results[OS_LINUX] + "<br>MacOS: "
+                + results[OS_MacOS];
+        res += "<p> Samsung: " + results[PHONE_SAMSUNG] + "<br>iPhone: " + results[PHONE_IPHONE] + "<br>Meizu: " + results[PHONE_MEIZU] +
+                "<br>Lenovo: " + results[PHONE_LENOVO] + "<br>Other : " + results[PHONE_OTHER] + "</p>";
+
+        resp.getWriter().println(String.format(TEMPLATE, res));
     }
 }
--- alina_sergeeva_web/src/main/webapp/index.jsp
+++ alina_sergeeva_web/src/main/webapp/index.jsp
@@ -1,12 +1,38 @@
-<html>
-<body>
-    <h2>Hello World!</h2>
-    <ol>
-        <li>Lexus</li>
-        <li>BMW</li>
-        <li>Infinity</li>
-    </ol>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
 
-    <a href="/alina_sergeeva_web_war_exploded/serv">Goto first servlet</a>
+<html>
+<head>
+    <title>Questionnaire</title>
+</head>
+<body>
+<form action="/alina_sergeeva_web_war_exploded/serv" method="post">
+    <p>
+        <b>Your name:</b><br>
+        <input type="text" size="40"><br>
+    </p>
+    <p>
+        <b>Your surname:</b><br>
+        <input type="text" size="40"><br>
+    </p>
+    <p>
+        <b>Your age:</b><br>
+        <input type="text" size="40"><br>
+    </p>
+    <p><b>Choose what kind of operating system you use::</b><br>
+        <input type="radio" name="os" value="windows"> WINDOWS<br>
+        <input type="radio" name="os" value="linux"> LINUX<br>
+        <input type="radio" name="os" value="macOs"> MacOS<br>
+
+    </p>
+    <p><b>Choose what brand of smartphone you have :</b><br>
+        <input type="radio" name="phone" value="samsung"> SAMSUNG<br>
+        <input type="radio" name="phone" value="iPhone"> IPHONE<br>
+        <input type="radio" name="phone" value="meizu"> MEIZU<br>
+        <input type="radio" name="phone" value="lenovo"> LENOVO<br>
+        <input type="radio" name="phone" value="other"> OTHER<br>
+    </p>
+    <p><input type="submit" value="Send">
+        </p>
+</form>
 </body>
 </html>
