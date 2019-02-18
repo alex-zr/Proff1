35d32bc89e8df0daa488d179f0d05457f9950273
Dmitry Pinchuk <fitanns@gmail.com>
2019-02-18 16:40:32 +0200

    homework jpa/hibernate 1

---
+++ dmitry_pinch/src/main/java/hwJpaHibernate1/Menu.java
@@ -0,0 +1,88 @@
+package hwJpaHibernate1;
+
+import javax.persistence.*;
+
+
+
+@Entity
+@Table(name = "MenuRest")
+@NamedQueries({
+        @NamedQuery(name = "Menu.findAll", query = "SELECT m FROM Menu m"),
+        @NamedQuery(name = "Menu.discount", query = "SELECT m FROM Menu m WHERE m.discount = :discount"),
+        @NamedQuery(name = "Menu.weight", query = "SELECT m FROM Menu m WHERE m.weight <= :weight"),
+        @NamedQuery(name = "Menu.fromTo",query = "SELECT m FROM Menu m WHERE m.price>:fr AND m.price<:to")
+
+})
+public class Menu {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @Column(nullable = false)
+    private String dish;
+    private double price;
+    private double weight;
+    private boolean discount;
+
+    public Menu() {
+    }
+
+    public Menu(String dish, double price, double weight, boolean discount) {
+        this.dish = dish;
+        this.price = price;
+        this.weight = weight;
+        this.discount = discount;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getDish() {
+        return dish;
+    }
+
+    public void setDish(String dish) {
+        this.dish = dish;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public boolean isDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(boolean discount) {
+        this.discount = discount;
+    }
+
+    @Override
+    public String toString() {
+        return "Menu{" +
+                "id=" + id +
+                ", dish='" + dish + '\'' +
+                ", price=" + price +
+                ", weight=" + weight +
+                ", discount=" + discount +
+                '}';
+    }
+}
